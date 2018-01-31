30304831ee2448951b1e209145a4fefb56f1a101
mwall <mwall@iastate.edu>
2018-01-30 18:04:30 -0600

    bug fixes with env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,25 +1,11 @@
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
-    apiKey: 'AIzaSyDhXG2wOc5ZmHVuvar16EsvEkK7KOohqkc',
-    authDomain: 'shop-e4aca.firebaseapp.com',
-    databaseURL: 'https://shop-e4aca.firebaseio.com',
-    projectId: 'shop-e4aca',
+    apiKey: 'AIzaSyAsx1bdloW9HwnHUZIOsqCzPU0Onz-PquU',
+    authDomain: 'paste-db.firebaseapp.com',
+    databaseURL: 'https://paste-db.firebaseio.com',
+    projectId: 'paste-db',
     storageBucket: '',
-    messagingSenderId: '425667628994'
+    messagingSenderId: '489762050414'
   }
 };
-
-
-//
-// export const environment = {
-//   production: false,
-//   firebase: {
-//     apiKey: 'AIzaSyAsx1bdloW9HwnHUZIOsqCzPU0Onz-PquU',
-//     authDomain: 'paste-db.firebaseapp.com',
-//     databaseURL: 'https://paste-db.firebaseio.com',
-//     projectId: 'paste-db',
-//     storageBucket: '',
-//     messagingSenderId: '489762050414'
-//   }
-// };
